103be45096d291fd489f78502372129b4cd7847b
unknown <rashadbayar@gmail.com>
2020-04-07 18:32:13 -0700

    update

--- script.js
+++ script.js
@@ -1,53 +1,64 @@
 
-$(document).ready(function() {
-
- let searchTerms = $("#searchBox").val();
+$(document).ready(function () {
 
 
- var APIKey = "302b9140c604266added2319618b8cb6";
+    $("#searchbutton").on("click", function (event) {
 
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=phoenix,Burundi&appid=" + APIKey;
-    
+        event.preventDefault();
 
- $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-   .then(function(response) {
+        var APIKey = "302b9140c604266added2319618b8cb6";
 
+        let searchTerms = $("#searchbox").val();
 
- // Log the queryURL
- console.log(queryURL);
+      
 
- // Log the resulting object
- console.log(response);
+        let searchBtn = document.querySelector("#searchbutton");
 
- // Transfer content to HTML
- $(".general-information").html("<h1>" + response.name + " Weather Details</h1>");
- $(".detail1").text("Wind Speed: " + response.wind.speed);
- $(".detail2").text("Humidity: " + response.main.humidity);
- 
- // Convert the temp to fahrenheit
- var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+       
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchTerms + "&appid=" + APIKey;  
 
- // add temp content to html
- $(".detail3").text("Temperature (K) " + response.main.temp);
+        console.log(searchTerms)
 
 
- // Log the data in the console as well
- console.log("Wind Speed: " + response.wind.speed);
- console.log("Humidity: " + response.main.humidity);
- console.log("Temperature (F): " + tempF);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+
+           console.log(response)
+
+           $.ajax({
+               url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + response.city.coord.lat + "&lon=" + response.city.coord.lon + "&appid=" + APIKey,
+               method: "GET"
+           }).then(function(response2){
+
+console.log(response2);
+           })
+           
+
+              /*   // Transfer content to HTM
+                $(".general-information").html("<h1>" + response.city.name + " Weather Details</h1>");
+                $(".detail1").text("Wind Speed: " + response.current.wind_speed);
+                $(".detail2").text("Humidity: " + response.current.humidity);
+
+                // Convert the temp to fahrenheit
+                var tempF = (response.main - 273.15) * 1.80 + 32;
+
+                // added temp content to html
+                $(".detail3").text("Temperature (K) " + response.current.temp);
+ */
+
+      
 
 
 
 
 
- 
-         }); 
+
+            });
+    })
+})
 
 
 
-
-
-});
