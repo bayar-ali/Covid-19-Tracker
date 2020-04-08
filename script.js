f9db8fa8d3d39a96ddf9561e68cb8e1990aedbf1
unknown <rashadbayar@gmail.com>
2020-04-08 13:58:03 -0700

    update

--- script.js
+++ script.js
@@ -10,12 +10,10 @@ $(document).ready(function () {
 
         let searchTerms = $("#searchbox").val();
 
-      
-
         let searchBtn = document.querySelector("#searchbutton");
 
-       
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchTerms + "&appid=" + APIKey;  
+
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchTerms + "&appid=" + APIKey;
 
         console.log(searchTerms)
 
@@ -26,30 +24,31 @@ $(document).ready(function () {
         })
             .then(function (response) {
 
-           console.log(response)
+                console.log(response)
 
-           $.ajax({
-               url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + response.city.coord.lat + "&lon=" + response.city.coord.lon + "&appid=" + APIKey,
-               method: "GET"
-           }).then(function(response2){
+                $.ajax({
+                    url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + response.city.coord.lat + "&lon=" + response.city.coord.lon + "&appid=" + APIKey,
+                    method: "GET"
+                }).then(function (response2) {
 
-console.log(response2);
-           })
-           
+                    console.log(response2);
 
-              /*   // Transfer content to HTM
+                })
+
+
+                // Transfer content to HTM
                 $(".general-information").html("<h1>" + response.city.name + " Weather Details</h1>");
-                $(".detail1").text("Wind Speed: " + response.current.wind_speed);
-                $(".detail2").text("Humidity: " + response.current.humidity);
+                $(".detail1").text("Wind Speed: " + response.list[0].wind.speed);
+                $(".detail2").text("Humidity: " + response.list[0].main.humidity);
 
-                // Convert the temp to fahrenheit
-                var tempF = (response.main - 273.15) * 1.80 + 32;
+           /*      // Convert the temp to fahrenheit
+                var tempF = (response.main - 273.15) * 1.80 + 32; */
 
                 // added temp content to html
-                $(".detail3").text("Temperature (K) " + response.current.temp);
- */
+                $(".detail3").text("Temperature (K) " + response.list[0].main.temp);
 
-      
+
+
 
 
 
